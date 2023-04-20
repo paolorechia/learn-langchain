eb71a2d939ec564d8b7233a462a0167076c3ec8a
Paolo Rechia <paolorechia@gmail.com>
2023-04-20 23:00:25 +0200

    hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,48 @@
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+# from alpaca_request_llm import AlpacaLLM
+from vicuna_request_llm import VicunaLLM
+
+# First, let's load the language model we're going to use to control the agent.
+llm = VicunaLLM()
+
+# Next, let's load some tools to use. Note that the `llm-math` tool uses an LLM, so we need to pass that in.
+tools = load_tools(['python_repl'], llm=llm)
+
+# Finally, let's initialize an agent with the tools, the language model, and the type of agent we want to use.
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+
+# Now let's test it out!
+agent.run("""
+For instance:
+
+Question: Find out how much 2 plus 2 is.
+Thought: I must use the Python shell to calculate 2 + 2
+Action: Python REPL
+Action Input: 
+2 + 2
+Observation: 4
+
+Thought: I now know the answer
+Final Answer: 4
+
+Example 2:
+Question: You have a variable age in your scope. If it's greater or equal than 21, say OK. Else, say Nay.
+Thought: I should write an if/else block in the Python shell.
+Action: Python REPL
+Action Input:
+if age >= 21:
+    print("OK")  # this line has four spaces at the beginning
+else:
+    print("Nay")  # this line has four spaces at the beginning
+
+Observation: OK
+Thought: I now know the answer
+Final Answer: I have executed the task successfully.
+
+Now begin for real!
+
+Question: Write a Python script that prints "Hello, world!"
+""")
+
