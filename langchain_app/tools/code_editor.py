f7c5b073ca51aa1ad10fac23cf66bc07a59af74c
Paolo Rechia <paolorechia@gmail.com>
2023-04-28 22:05:44 +0200

    Implement run/add

--- langchain_app/tools/code_editor.py
+++ langchain_app/tools/code_editor.py
@@ -1,5 +1,7 @@
 from langchain.agents import Tool
 from pydantic import BaseModel, Field
+import tempfile
+import subprocess
 
 from typing import List
 
@@ -19,17 +21,29 @@ class CodeEditorTooling:
     def __init__(self) -> None:
         self.source_code: List[str] = []
 
-    def add_code(add_code_input: CodeEditorAddCodeInput):
+    def add_code(self, add_code_input: CodeEditorAddCodeInput):
+        new_lines_of_code =  [line for line in add_code_input.input_code.split("\n") if line]
+        self.source_code.extend(new_lines_of_code)
+
+    def change_code_line(self, change_code_line_input: CodeEditorChangeCodeLineInput):
         pass
 
-    def change_code_line(change_code_line_input: CodeEditorChangeCodeLineInput):
-        pass
-
-    def delete_code_lines(delete_code_lines_inpuot: CodeEditorDeleteCodeLinesInput):
+    def delete_code_lines(self, delete_code_lines_input: CodeEditorDeleteCodeLinesInput):
         pass
 
     def run_code(self):
-        pass
+        with tempfile.NamedTemporaryFile(buffering=0) as fp:
+            lines_to_save = [line.encode("utf-8") for line in self.source_code]
+            fp.writelines(lines_to_save)
+            print("Source saved to file: ", fp.name)    
+
+            completed_process = subprocess.run(["python3", fp.name], capture_output=True, timeout=10)
+
+            print(completed_process, completed_process.stderr)
+            succeeded = "Succeeded" if completed_process.returncode == 0 else "Failed"
+            stdout = completed_process.stdout
+            stderr = completed_process.stderr
+            return f"Program {succeeded}\nStdout:{stdout}\nStderr:{stderr}"
 
     def build_add_code_tool(self):
         return Tool(
@@ -38,7 +52,7 @@ class CodeEditorTooling:
             description="useful for when you need to execute Python code",
         )
 
-    def build_change_code_line_toold(self):
+    def build_change_code_line_tool(self):
         return Tool(
             name="CodeEditorChangeCodeLine",
             func=self.change_code_line,
@@ -48,13 +62,13 @@ class CodeEditorTooling:
     def build_delete_code_lines_tool(self):
         return Tool(
             name="CodeEditorDeleteLine",
-            func=self.add_code,
+            func=self.delete_code_lines,
             description="useful for when you need to execute Python code",
         )
 
     def build_run_tool(self):
         return Tool(
             name="CodeEditorRunCode",
-            func=self.add_code,
+            func=self.run_code,
             description="useful for when you need to execute Python code",
         )
--- langchain_app/tools/test_code_editor.py
+++ langchain_app/tools/test_code_editor.py
@@ -1,7 +1,35 @@
 import pytest
-from langchain_app.tools.code_editor import CodeEditorTooling
+from langchain_app.tools.code_editor import CodeEditorTooling, CodeEditorAddCodeInput, CodeEditorChangeCodeLineInput, CodeEditorDeleteCodeLinesInput
 
-def test_code_editor():
-
+def test_code_editor_init():
     code_editor = CodeEditorTooling()
-    
+    assert code_editor
+
+
+def test_code_run():
+    code_editor = CodeEditorTooling()
+    code_editor.source_code = ["print('hello, world')"]
+    result = code_editor.run_code()
+    succeeded = "Succeeded"
+    stdout = b'hello, world\n'
+    stderr = b''
+    assert result == f"Program {succeeded}\nStdout:{stdout}\nStderr:{stderr}"
+
+
+def test_code_run_exception():
+    code_editor = CodeEditorTooling()
+    code_editor.source_code = ["print('hello, world'"]
+    result = code_editor.run_code()
+    assert "Program Failed" in result
+    assert "SyntaxError" in result
+
+
+def test_code_add_code():
+    code_editor = CodeEditorTooling()
+    add_input = CodeEditorAddCodeInput(
+input_code="""print('hello')
+print('world')
+"""
+)
+    code_editor.add_code(add_input)
+    assert code_editor.source_code == ["print('hello')", "print('world')"]
