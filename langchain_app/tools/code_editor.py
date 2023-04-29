04fa3a96cb3fa8dd26f5c56853d4fd054bab54f3
Paolo Rechia <paolorechia@gmail.com>
2023-04-29 22:35:52 +0200

    Add codeload flag

--- langchain_app/tools/code_editor.py
+++ langchain_app/tools/code_editor.py
@@ -1,9 +1,9 @@
 from langchain.agents import Tool
 from pydantic import BaseModel, Field
-import tempfile
+import random
 import subprocess
 
-from typing import List
+from typing import List, Optional
 
 class CodeEditorChangeCodeLineInput(BaseModel):
     input_code: str = Field()
@@ -14,11 +14,19 @@ class CodeEditorDeleteCodeLinesInput(BaseModel):
 
 
 class CodeEditorTooling:
-    def __init__(self) -> None:
+    def __init__(self, filename: Optional[str] = None, load_source_code_on_start=False) -> None:
         self.source_code: List[str] = []
+        self.filename = filename
+        if not self.filename:
+            self.filename = [random.choice("abcdefghijklmnoqrstuvxwyz") for r in range(10)]
+            self.filename += ".py"
+        print("Init code editor with filename: ", self.filename)
+        if load_source_code_on_start:
+            print("Loading existing source code")
+            with open(self.filename, "r") as fp:
+                self.source_code = fp.readlines()
 
     def add_code(self, add_code_input: str):
-        print("Adding code: ", add_code_input)
         new_lines_of_code =  [line for line in add_code_input.split("\n") if line]
         self.source_code.extend(new_lines_of_code)
 
@@ -36,23 +44,14 @@ class CodeEditorTooling:
         for line in lines_to_delete:
             idx = line -1
             self.source_code.pop(idx)
-        # return self.display_code()
 
 
     def run_code(self, *args, **kwargs):
         filename = "persistent_source.py"
         with open(filename, "w") as fp:
-        # with tempfile.NamedTemporaryFile(buffering=0) as fp:
-            # lines_to_save = [line.encode("utf-8") for line in self.source_code]
-            # fp.writelines(lines_to_save)
-            # filename = fp.name
-
             fp.write("\n".join(self.source_code))
-            print("Source saved to file: ", filename)
 
         completed_process = subprocess.run(["python3", filename], capture_output=True, timeout=10)
-
-        print(completed_process, completed_process.stderr)
         succeeded = "Succeeded" if completed_process.returncode == 0 else "Failed"
         stdout = completed_process.stdout
         stderr = completed_process.stderr
@@ -60,7 +59,7 @@ class CodeEditorTooling:
         
     def display_code(self):
         code_string = ""
-        for idx, line in enumerate(self.source_code):
+        for _, line in enumerate(self.source_code):
             code_string += f"{line}\n"
         return code_string
 
