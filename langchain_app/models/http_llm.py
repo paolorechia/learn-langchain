2c78f17538356eaf74069b65bf87d04160d7efbd
Paolo Rechia <paolorechia@gmail.com>
2023-04-30 21:20:18 +0200

    Fix langchain passed stop token with http llm

--- langchain_app/models/http_llm.py
+++ langchain_app/models/http_llm.py
@@ -3,7 +3,7 @@ from typing import Optional, List, Mapping, Union, Any, Callable
 from pydantic import Field
 from typing import Dict
 import requests
-
+from copy import deepcopy
 
 def default_extractor(json_response: Dict[str, Any]) -> str:
     return json_response["response"]
@@ -15,20 +15,25 @@ class HTTPBaseLLM(LLM):
         default_factory=dict
     )
     response_extractor: Callable[[Dict[str, Any]], str] = default_extractor
+    stop_parameter_name: str = "stop"
 
     @property
     def _llm_type(self) -> str:
         return "custom"
 
     def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        # Merge passed stop list with class parameters
         if isinstance(stop, list):
-            stop = stop + ["Observation:"]
+            stop_list = list(set(stop).union(set(self.parameters[self.stop_parameter_name])))
+
+        params = deepcopy(self.parameters)
+        params[self.stop_parameter_name] = stop_list
 
         response = requests.post(
             self.prompt_url,
             json={
                 "prompt": prompt,
-                **self.parameters,
+                **params,
             },
         )
         response.raise_for_status()
