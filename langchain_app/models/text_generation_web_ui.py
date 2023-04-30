37b9a664fcfdfce583dade524f8eceb8a1209259
Paolo Rechia <paolorechia@gmail.com>
2023-04-30 23:05:38 +0200

    Force stop token removal for ReAct on text generation web UI

--- langchain_app/models/text_generation_web_ui.py
+++ langchain_app/models/text_generation_web_ui.py
@@ -1,16 +1,16 @@
 from langchain_app.models.http_llm import HTTPBaseLLM
 
-
+STOP_TOKEN = "Observation"
 def default_parameters():
     return {
         "max_new_tokens": 250,
         "do_sample": True,
-        "temperature": 1.3,
+        "temperature": 0.001,
         "top_p": 0.1,
         "typical_p": 1,
-        "repetition_penalty": 1.18,
-        "top_k": 40,
-        "min_length": 0,
+        "repetition_penalty": 1.2,
+        "top_k": 1,
+        "min_length": 32,
         "no_repeat_ngram_size": 0,
         "num_beams": 1,
         "penalty_alpha": 0,
@@ -21,12 +21,14 @@ def default_parameters():
         "truncation_length": 2048,
         "ban_eos_token": False,
         "skip_special_tokens": True,
-        "stopping_strings": ["Observation:"],
+        "stopping_strings": [STOP_TOKEN + ":"],
     }
 
 
 def response_extractor(json_response):
     result = json_response["results"][0]["text"]
+    if STOP_TOKEN in result:
+        return result.split(STOP_TOKEN)[0]
     return result
 
 
