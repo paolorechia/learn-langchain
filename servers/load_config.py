5c3b39ba481108c167e035bbffd9cac445c1614e
TonyTang <60964665+bigjeager@users.noreply.github.com>
2023-05-06 15:12:16 +0800

    Update load_config.py (#20)
    os.getenv() always return string type

--- servers/load_config.py
+++ servers/load_config.py
@@ -3,12 +3,12 @@ import os
 
 class Config:
     def __init__(self) -> None:
-        self.base_model_size = "13b" if os.getenv("USE_13B_MODEL") else "7b"
-        self.use_4bit = True if os.getenv("USE_4BIT") else False
-        self.use_8bit = True if os.getenv("USE_8BIT") else False
-        self.use_fine_tuned_lora = True if os.getenv("USE_FINE_TUNED_LORA") else False
+        self.base_model_size = "13b" if os.getenv("USE_13B_MODEL") == 'true' else "7b"
+        self.use_4bit = True if os.getenv("USE_4BIT") == 'true' else False
+        self.use_8bit = True if os.getenv("USE_8BIT") == 'true' else False
+        self.use_fine_tuned_lora = True if os.getenv("USE_FINE_TUNED_LORA") == 'true' else False
         self.lora_weights = os.getenv("LORA_WEIGHTS")
-        self.device = "cpu" if os.getenv("USE_CPU") else "cuda"
+        self.device = "cpu" if os.getenv("USE_CPU")== 'true' else "cuda"
         self.model_path = os.getenv("MODEL_PATH")
         self.model_checkpoint = os.getenv("MODEL_CHECKPOINT")
 
