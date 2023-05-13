110e0de2caafdffd05b11d86fcd6dd80199c4783
Paolo Rechia <paolorechia@gmail.com>
2023-05-13 14:18:28 +0200

    Fine tune example

---
+++ langchain_app/agents/finetuned_hello.py
@@ -0,0 +1,19 @@
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+
+from langchain_app.models.vicuna_request_llm import VicunaLLM
+
+# First, let's load the language model we're going to use to control the agent.
+llm = VicunaLLM()
+
+# Next, let's load some tools to use. Note that the `llm-math` tool uses an LLM, so we need to pass that in.
+tools = load_tools(["python_repl"], llm=llm)
+
+# Finally, let's initialize an agent with the tools, the language model, and the type of agent we want to use.
+agent = initialize_agent(
+    tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+)
+
+# Now let's test it out!
+agent.run("""Write a Python script that prints 'Hello, world!""")
